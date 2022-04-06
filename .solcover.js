c932f1ee954aa51b8d83f127d1baa0701cdce91e
ahmedali <imahmedalibhatti@gmailcom>
2022-04-06 22:14:02 +0500

    chore: upgrade files to yaml

--- .solcover.js
+++ .solcover.js
@@ -2,9 +2,5 @@ const shell = require("shelljs");
 
 module.exports = {
   istanbulReporter: ["html", "lcov"],
-  onIstanbulComplete: async function (_config) {
-    // We need to do this because solcover generates bespoke artifacts.
-    shell.rm("-rf", "./artifacts");
-  },
-  skipFiles: ["mocks", "test"],
+  skipFiles: ["test"],
 };
