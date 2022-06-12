3c4bcc546eb4bab570c4e60e3d4c1eb5f63bd576
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-06-12 13:06:05 +0500

    fix: rename network api keys for etherscan

--- config/api-keys.ts
+++ config/api-keys.ts
@@ -1,39 +1,38 @@
 import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
 
-import { NetworkName } from "./networks";
-
 dotenvConfig({ path: resolve(__dirname, "../.env") });
 
 export const API_KEYS: string | Record<string, string> | undefined = {
   // ETHEREUM
-  [NetworkName.MAINNET]: process.env.ETHERSCAN_API_KEY || "",
-  [NetworkName.KOVAN]: process.env.ETHERSCAN_API_KEY || "",
-  [NetworkName.GOERLI]: process.env.ETHERSCAN_API_KEY || "",
-  [NetworkName.RINKEBY]: process.env.ETHERSCAN_API_KEY || "",
-  [NetworkName.ROPSTEN]: process.env.ETHERSCAN_API_KEY || "",
+  mainnet: process.env.ETHERSCAN_API_KEY || "",
+  kovan: process.env.ETHERSCAN_API_KEY || "",
+  goerli: process.env.ETHERSCAN_API_KEY || "",
+  rinkeby: process.env.ETHERSCAN_API_KEY || "",
+  ropsten: process.env.ETHERSCAN_API_KEY || "",
+  sepolia: process.env.ETHERSCAN_API_KEY || "",
 
   // BINANCE SMART CHAIN
-  [NetworkName.BSC]: process.env.BSCSCAN_API_KEY || "",
-  [NetworkName.BSC_TESTNET]: process.env.BSCSCAN_API_KEY || "",
+  bsc: process.env.BSCSCAN_API_KEY || "",
+  bscTestnet: process.env.BSCSCAN_API_KEY || "",
 
   // MATIC/POLYGON
-  [NetworkName.POLYGON_MAINNET]: process.env.POLYGONSCAN_API_KEY || "",
-  [NetworkName.POLYGON_MUMBAI]: process.env.POLYGONSCAN_API_KEY || "",
+  polygon: process.env.POLYGONSCAN_API_KEY || "",
+  polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
 
   // OPTIMISM
-  [NetworkName.OPTIMISM_MAINNET]: process.env.OPTIMISM_API_KEY || "",
-  [NetworkName.OPTIMISM_KOVAN]: process.env.OPTIMISM_API_KEY || "",
+  optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
+  optimisticKovan: process.env.OPTIMISM_API_KEY || "",
 
   // ARBITRUM
-  [NetworkName.ARBITRUM_MAINNET]: process.env.ARBISCAN_API_KEY || "",
-  [NetworkName.ARBITRUM_RINKEBY]: process.env.ARBISCAN_API_KEY || "",
+  arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+  arbitrumTestnet: process.env.ARBISCAN_API_KEY || "",
 
   // AVALANCHE
-  [NetworkName.AVALANCHE_MAINNET]: process.env.SNOWTRACE_API_KEY || "",
-  [NetworkName.FUJI_AVALANCHE]: process.env.SNOWTRACE_API_KEY || "",
+  avalanche: process.env.SNOWTRACE_API_KEY || "",
+  avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY || "",
 
   // FANTOM
-  [NetworkName.FANTOM_MAINNET]: process.env.FANTOM_API_KEY || "",
-  [NetworkName.FANTOM_TESTNET]: process.env.FANTOM_API_KEY || "",
+  opera: process.env.FANTOM_API_KEY || "",
+  ftmTestnet: process.env.FANTOM_API_KEY || "",
 };
