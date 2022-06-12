d4c9642db01ca4cece2855a560de090bfba75067
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-06-12 15:02:45 +0500

    feat: add remove helper in utils/files.ts

--- utils/files.ts
+++ utils/files.ts
@@ -36,3 +36,19 @@ export const parseFile = async (filePath: string): Promise<any> => {
 
   return null;
 };
+
+export const deleteFolderRecursive = (directoryPath: string) => {
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach((file) => {
+      const curPath = path.join(directoryPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(directoryPath);
+  }
+};
